fa5fa099167111b70185642121204856e99dcfdf
abelmathew07 <abelmathewyo22@gmail.com>
2024-11-12 12:18:21 +0530

    added sreyas and minhaj's code

--- Phase 1/Day4/Task1/temperature.cpp
+++ Phase 1/Day4/Task1/temperature.cpp
@@ -4,27 +4,27 @@ using namespace std;
 #include<cstdlib>
 
 
-
 void readTemperature(int temperatures[],int& size)
 {
        int i=0; //for iteration
        int currentTemperature=0;
-       while(true)
+       do
         {
             
          cout << "enter the patient's temperature " << (i+1) << ": ";
          cin >> currentTemperature;
-           if (currentTemperature!=0)
-           {
-                ++size;
+            if (currentTemperature!=0)
+            {    ++size;
                 temperatures[i] = currentTemperature;
                 ++i;
-           }  
-          
+            }
+                
         }
+          while(currentTemperature!=0); 
+          
+        
  
 }  
-
 int findTemperatureSum(int temperatures[] , int size )
 {
     int sum = 0;
@@ -41,6 +41,50 @@ int findTemperatureAverage(int temperatureSum , int size)
 }
 
 
+bool isOddTemperature(int number) 
+{
+    if (number % 2 != 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int findSumOfOddTemperature(int temperatures[] , int size)
+{
+    int oddSum = 0;
+    int number;
+    
+    for (int i = 0; i <= size-1; i++)
+    {    
+        number = temperatures[i];
+        if (isOddTemperature(number))
+        {
+            oddSum = (oddSum + number);
+        }
+    }
+    
+    return oddSum;
+}
+
+
+
+int findMinntemperature(int temperatures[],int size)
+{
+    int minTemp = MAX_SIZE;
+    for(int i =0;i<size;i++)
+    {
+         if(temperatures[i] < minTemp)
+        {
+            minTemp = temperatures[i];
+        }
+    }
+    return minTemp;
+}
+
 
 
 void Test()
@@ -49,15 +93,16 @@ void Test()
     int size = 0;
 
     readTemperature(temperatures , size);
-    int temperatureSum =findTemperatureSum(temperatures , size);
-    int averageTemperature=findTemperatureAverage(temperatureSum , size);
-    
-    
+    int temperatureSum     = findTemperatureSum(temperatures , size);
+    int averageTemperature = findTemperatureAverage(temperatureSum , size);
+    int sumOfOddTemperature = findSumOfOddTemperature(temperatures , size);
+    int minimumTemperature = findMinntemperature(temperatures , size);
 
 
 
     cout << "The Average Temperature is: " << averageTemperature << endl;
-
+    cout << "Minimum Temperature is: " << minimumTemperature << endl;
+    cout << "Sum of Odd Temperatures is: " << sumOfOddTemperature << endl;
     
 }
 
